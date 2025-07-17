a344735d92be71bfc0bc2b484f51772dd7877b59
marsxnow <raxelportiz@gmail.com>
2025-07-17 18:55:40 -0400

    Initial Commit

---
+++ GR Study/GR_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GR_StudyApp.swift
+//  GR Study
+//
+//  Created by rax  on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GR_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GR StudyTests/GR_StudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GR_StudyTests.swift
+//  GR StudyTests
+//
+//  Created by rax  on 7/17/25.
+//
+
+import Testing
+@testable import GR_Study
+
+struct GR_StudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
